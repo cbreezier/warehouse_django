d19b97433944da257061c59098341b25a901c912
Leo Huang <crazysheep2302@gmail.com>
2014-07-21 17:10:04 +1000

    Modify sample bay view

--- warehouse/static/warehouse/js/draw.js
+++ warehouse/static/warehouse/js/draw.js
@@ -1,7 +1,27 @@
+selectedBay = parseInt(Math.random() * 31) + 1;
+selectedPallet = parseInt(Math.random() * 8) + 1;
+palletData = new Array(31);
+for (var i = 0; i < 31; i++) {
+    palletData[i] = new Array(8);
+    for (var j = 0; j < 8; j++) {
+        palletData[i][j] = Math.random() * 100;
+    }
+}
+
 $(document).ready(function() {
-    /*
-     * Birds-eye view
-     */
+    renderBirdView(selectedBay);
+    renderGroundView(selectedBay, selectedPallet, palletData);
+});
+
+$(window).resize(function() {
+    renderBirdView(selectedBay);
+    renderGroundView(selectedBay, selectedPallet, palletData);
+});
+
+/*
+ * Birds-eye view
+ */
+function renderBirdView(selectedBay) {
     var birdview = document.getElementById('birdview');
     birdview.width = birdview.parentNode.offsetWidth;
     birdview.height = birdview.parentNode.offsetHeight;
@@ -31,30 +51,61 @@ $(document).ready(function() {
     birdview_c.rect(margin, margin + (7 * shelfWidth), birdview.width - (2 * margin), shelfWidth);
     birdview_c.fill();
     birdview_c.stroke();
+}
 
-    /*
-     * Mans-eye view
-     */
+/*
+ * Mans-eye view
+ */
+function renderGroundView(selectedBay, selectedPallet) {
     var groundview = document.getElementById('groundview');
     groundview.width = groundview.parentNode.offsetWidth;
     groundview.height = groundview.parentNode.offsetHeight;
     var groundview_c = groundview.getContext('2d');
 
-    var offsetY = groundview.height * 0.15;
+    if (selectedBay == 0) {
+        groundview_c.fillStyle = '#DDDDDD';
+        groundview_c.font = (groundview.width / 20) + 'px Arial';
+        groundview_c.textAlign = 'center';
+        groundview_c.fillText('No bay selected',
+                              groundview.width / 2,
+                              groundview.height / 2);
+        return;
+    }
 
-    groundview_c.font = '30pt Arial';
+    var margin = 20;
+    var offsetY = groundview.height * 0.15;
+    var dy = groundview.height * 0.8 / 4;
+    var dx = (groundview.width - (2 * margin)) / 2;
+
+    groundview_c.fillStyle = 'black';
+    groundview_c.font = 0.5 * offsetY + 'px Arial';
     groundview_c.textAlign = 'center';
-    groundview_c.fillText('Bay 4', groundview.width / 2, offsetY - 10);
+    groundview_c.fillText('Bay ' + selectedBay, groundview.width / 2, offsetY - 20);
 
     groundview_c.strokeStyle = 'gray';
+    groundview_c.lineWidth = 2;
+    groundview_c.fillStyle = 'orange';
+    for (var row = 0; row < 4; row++) {
+        for (var col = 0; col < 2; col++) {
+            var palletNumber = (row * 2) + col + 1;
+            var fillHeight = palletData[selectedBay - 1][palletNumber - 1] * dy / 100;
+            groundview_c.fillRect(startX+1, startY + dy - fillHeight+1, dx-2, fillHeight-1);
 
-    groundview_c.beginPath();
-    var dy = groundview.height * 0.55 / 4;
-    for (var i = 1; i <= 4; i++) {
-        groundview_c.moveTo(0, offsetY + (i * dy));
-        groundview_c.lineTo(groundview.width, offsetY + (i * dy));
+            var startX = margin + (col * dx);
+            var startY = offsetY + (row * dy);
+            groundview_c.strokeRect(startX, startY, dx, dy);
+        }
     }
-    groundview_c.moveTo(groundview.width / 2, offsetY);
-    groundview_c.lineTo(groundview.width / 2, offsetY + (4 * dy));
-    groundview_c.stroke();
-});
+
+    // groundview_c.strokeStyle = 'gray';
+
+    // groundview_c.beginPath();
+    // var dy = groundview.height * 0.8 / 4;
+    // for (var i = 1; i <= 4; i++) {
+    //     groundview_c.moveTo(margin, offsetY + (i * dy));
+    //     groundview_c.lineTo(groundview.width - margin, offsetY + (i * dy));
+    // }
+    // groundview_c.moveTo(groundview.width / 2, offsetY);
+    // groundview_c.lineTo(groundview.width / 2, offsetY + (4 * dy));
+    // groundview_c.stroke();
+}
